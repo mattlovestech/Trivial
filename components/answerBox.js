4ea622240e00ba5952ceb09b55ade4ba096decd7
Matthew Manning <matthewmanning2014@gmail.com>
2022-02-20 21:01:08 -0500

    added progress bars

--- components/answerBox.js
+++ components/answerBox.js
@@ -21,10 +21,11 @@ export default class AnswerBox extends React.Component {
                         borderTopLeftRadius: "25px", borderTopRightRadius: "25px"}}>
                         <h3>What city is Howard University located in?</h3>
                     </Box>
+
                     <Box style={{textAlign: "center",
                         background: "rgba(255, 255, 255, 0.3)",
                         borderBottomLeftRadius: "25px", borderBottomRightRadius: "25px"}}>
-                        <br/>
+
                         <Button className={blankQuizButton}
 
                                 variant={"contained"}
@@ -32,14 +33,17 @@ export default class AnswerBox extends React.Component {
                             <Avatar sx={{ background: "white", color: "black", border: "1px black solid", width: 24, height: 24, marginRight: "15px"}} alt="A" src="/static/images/avatar/1.jpg" />
                             <a>Atlanta</a>
                         </Button>
+                        <br/>
                         <Button  className={blankQuizButton} variant={"contained"}>
                             <Avatar sx={{ background: "white", color: "black",border: "1px black solid",width: 24, height: 24, marginRight: "15px"}} alt="B" src="/static/images/avatar/1.jpg" />
                             <a>Washington D.C.</a>
                         </Button>
+                        <br/>
                         <Button  className={blankQuizButton} variant={"contained"} >
                             <Avatar sx={{ background: "white", color: "black",border: "1px black solid",width: 24, height: 24, marginRight: "15px"}} alt="C" src="/static/images/avatar/1.jpg" />
                             <a>Houston</a>
                         </Button>
+                        <br/>
                         <Button  className={blankQuizButton} variant={"contained"} >
                             <Avatar sx={{ background: "white", color: "black",border: "1px black solid",width: 24, height: 24, marginRight: "15px"}} alt="D" src="/static/images/avatar/1.jpg" />
                             <a>Houston</a>
