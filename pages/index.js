83d49f6b7fa74c1fbfdd08b94d1beed1925b4245
Matthew Manning <matthewmanning2014@gmail.com>
2022-02-20 19:46:50 -0500

    updated tagline for next

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Create a shareable quiz for anything
         </h1>
 
         <p className={styles.description}>
